e9d684f36a9c150f8872eae9c4aa6c6378994405
qifenghao <liu_qifeng1991@163.com>
2021-01-29 15:34:37 +0800

    调整代码格式，消除警告

--- src/main/java/my/qifeng/chatroom/sip/ChatRoomSipServlet.java
+++ src/main/java/my/qifeng/chatroom/sip/ChatRoomSipServlet.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ChatRoomSipServlet extends SipServlet {
@@ -29,13 +28,14 @@ public class ChatRoomSipServlet extends SipServlet {
      */
     public void init() throws ServletException {
         super.init(); // TODO Remove
-        getServletContext().setAttribute(CHATROOM_USER_LIST_KEY, new ArrayList());
+        getServletContext().setAttribute(CHATROOM_USER_LIST_KEY, new ArrayList<String>());
         String serverName = getServletConfig().getInitParameter(CHATROOM_SERVER_NAME_KEY);
         int serverPort = Integer.parseInt(getServletConfig().getInitParameter(CHATROOM_SERVER_PORT_KEY));
-        String ip = null;
+        String ip;
         try {
             ip = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
+            e.printStackTrace();
             throw new ServletException(e);
         }
         serverAddress = String.format("sip:%s@%s:%d", serverName, ip, serverPort);
@@ -45,47 +45,44 @@ public class ChatRoomSipServlet extends SipServlet {
      *  This is called by the container when shutting down the service.
      */
     public void destroy() {
-        try
-        {
+        try {
             sendToAll(serverAddress, "Server is shutting down -- goodbye!");
-        } catch (Throwable e)
-        { //ignore all errors when shutting down.
+        } catch (Throwable e) {
+            // Ignore all errors when shutting down.
             e.printStackTrace();
         }
-        super.destroy();
+        super.destroy(); // TODO Remove
     }
 
-    /** This is called by the container when a MESSAGE message arrives. */
-    protected void doMessage(SipServletRequest request) throws
-            ServletException, IOException {
+    /**
+     *  This is called by the container when a MESSAGE message arrives.
+     */
+    protected void doMessage(SipServletRequest request) throws ServletException, IOException {
 
         request.createResponse(SipServletResponse.SC_OK).send();
 
         String message = request.getContent().toString();
         String from = request.getFrom().toString();
 
-        //A user asked to quit.
-        if(message.equalsIgnoreCase("/quit")) {
+        // A user asked to quit.
+        if("/quit".equalsIgnoreCase(message)) {
             sendToUser(from, "Bye");
             removeUser(from);
             return;
         }
 
-        //Add user to the list
+        // Add user to the list
         if(!containsUser(from)) {
-            sendToUser(from, "Welcome to chatroom " + serverAddress +
-                    ". Type '/quit' to exit.");
+            sendToUser(from, "Welcome to chatroom " + serverAddress + ". Type '/quit' to exit.");
             addUser(from);
         }
 
-        //If the user is joining the chat room silently, no message
-        //to broadcast, return.
-        if(message.equalsIgnoreCase("/join")) {
+        // If the user is joining the chat room silently, no message to broadcast.
+        if("/join".equalsIgnoreCase(message)) {
             return;
         }
 
-        //We could implement more IRC commands here,
-        //see http://www.mirc.com/cmds.html
+        // Broadcast
         sendToAll(from, message);
     }
 
@@ -93,11 +90,9 @@ public class ChatRoomSipServlet extends SipServlet {
      * This is called by the container when an error is received
      * regarding a sent message, including timeouts.
      */
-    protected void doErrorResponse(SipServletResponse response)
-            throws ServletException, IOException {
-        super.doErrorResponse(response);
-        //The receiver of the message probably dropped off. Remove
-        //the receiver from the list.
+    protected void doErrorResponse(SipServletResponse response) throws ServletException, IOException {
+        super.doErrorResponse(response); // TODO Remove
+        // The receiver of the message probably dropped off. Remove the receiver from the list.
         String receiver = response.getTo().toString();
         removeUser(receiver);
     }
@@ -106,56 +101,50 @@ public class ChatRoomSipServlet extends SipServlet {
      * This is called by the container when a 2xx-OK message is
      * received regarding a sent message.
      */
-    protected void doSuccessResponse(SipServletResponse response)
-            throws ServletException, IOException {
-        super.doSuccessResponse(response);
-        //We created the app session, we have to destroy it too.
+    protected void doSuccessResponse(SipServletResponse response) throws ServletException, IOException {
+        super.doSuccessResponse(response); // TODO Remove
+        // We created the app session, we have to destroy it too.
         response.getApplicationSession().invalidate();
     }
 
-    private void sendToAll(String from, String message)
-            throws ServletParseException, IOException {
-        SipFactory factory = (SipFactory)getServletContext().
-                getAttribute("javax.servlet.sip.SipFactory");
+    @SuppressWarnings("unchecked")
+    private void sendToAll(String from, String message) throws ServletParseException, IOException {
+        SipFactory factory = (SipFactory) getServletContext().getAttribute("javax.servlet.sip.SipFactory");
 
-        List list = (List)getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
-        Iterator users = list.iterator();
-        while (users.hasNext()) { //Send this message to all on the list.
-            String user = (String) users.next();
-
-            SipApplicationSession session =
-                    factory.createApplicationSession();
-            SipServletRequest request = factory.createRequest(session,
-                    "MESSAGE", serverAddress, user);
+        // Send this message to all users.
+        List<String> userList = (List<String>) getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
+        for (String user : userList) {
+            SipApplicationSession session = factory.createApplicationSession();
+            SipServletRequest request = factory.createRequest(session, "MESSAGE", serverAddress, user);
             String msg = from + " sent message: \n" + message;
             request.setContent(msg.getBytes(), "text/plain");
             request.send();
         }
     }
 
-    private void sendToUser(String to, String message)
-            throws ServletParseException, IOException {
-        SipFactory factory = (SipFactory)getServletContext().
-                getAttribute("javax.servlet.sip.SipFactory");
+    private void sendToUser(String to, String message) throws ServletParseException, IOException {
+        SipFactory factory = (SipFactory) getServletContext().getAttribute("javax.servlet.sip.SipFactory");
         SipApplicationSession session = factory.createApplicationSession();
-        SipServletRequest request = factory.createRequest(session,
-                "MESSAGE", serverAddress, to);
+        SipServletRequest request = factory.createRequest(session, "MESSAGE", serverAddress, to);
         request.setContent(message.getBytes(), "text/plain");
         request.send();
     }
 
+    @SuppressWarnings("unchecked")
     private boolean containsUser(String from) {
-        List list = (List)getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
+        List<String> list = (List<String>) getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
         return list.contains(from);
     }
 
+    @SuppressWarnings("unchecked")
     private void addUser(String from) {
-        List list = (List)getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
+        List<String> list = (List<String>) getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
         list.add(from);
     }
 
+    @SuppressWarnings("unchecked")
     private void removeUser(String from) {
-        List list = (List)getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
+        List<String> list = (List<String>) getServletContext().getAttribute(CHATROOM_USER_LIST_KEY);
         list.remove(from);
     }
 }
